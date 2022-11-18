a03e2f266779d3d0cbeda279e18f42a7a15c5fd7
TehTemM@gmail.com <TehTemM@gmail.com>
2022-11-18 17:01:14 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Chemeris_PR6._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (pwBox.Password.ToString() == "1" && logBox.Password.ToString() == "1")
+            {
+                statusText.Text = "Верный bruh";
+Window1 window1 = new Window1();
+                window1.Show();
+            }
+            else if (pwBox.Password.ToString() != "1")
+            {
+                MessageBox.Show("Неверный пароль");
+            }
+            else if (logBox.Password.ToString() != "1")
+{
+    MessageBox.Show("Не найден логин");
+}
+        }
+    }
+}
